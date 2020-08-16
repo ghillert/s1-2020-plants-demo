94eca1178c2a8bec95b86f1172c6454b192f4dc5
Gunnar Hillert <gunnar@hillert.com>
2020-08-15 22:36:43 -1000

    Add Spring Initializer project

---
+++ src/test/java/com/hillert/s1/plants/PlantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hillert.s1.plants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
